a114c0be616839ef83b4671321a68c7bb5f4b62f
RachelAbowitch <racheli226939@gmail.com>
2023-12-25 12:47:40 +0200

    trip schema change

--- server/models/Trip.mode.js
+++ server/models/Trip.mode.js
@@ -1,35 +1,33 @@
-
-
 const mongoose = require('mongoose');
-
-// Define the schema for landmarks in the route
-const landmarkSchema = new mongoose.Schema({
-  location: String,
-  landmarkName: String,
-  description: String,
-  startTime: String,
-  length: String,
-  nextLandmarkId: Number
-});
-
-// Define the schema for equipment
-const equipmentSchema = new mongoose.Schema({
-  userName: String,
-  productName: String,
-  productId: Number
-});
 
 // Define the main trip schema
 const tripSchema = new mongoose.Schema({
   tripId: Number,
   tripName: String,
   guideId: String,
-  travelersList: [{
-    userId: Number,
-    username: String
-  }],
-  route: [landmarkSchema],
-  equipmentList: [equipmentSchema]
+  travelersList: [
+    {
+      userId: Number,
+      username: String
+    }
+  ],
+  route: [
+    {
+      location: String,
+      landmarkName: String,
+      description: String,
+      startTime: String,
+      length: String,
+      nextLandmarkId: Number
+    }
+  ],
+  equipmentList: [
+    {
+      userName: String,
+      productName: String,
+      productId: Number
+    }
+  ]
 });
 
 // Create the model for the Trip collection
