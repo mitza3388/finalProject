3d82ae19e510a49f9f872d9a5e6d8efed97b28f8
yael <y0583246419@gmail.com>
2023-12-26 14:03:17 +0200

    add getParticipantsByTripId, getRouteByTripId, getEquipmentListByTripId

--- server/controllers/trip.controller.js
+++ server/controllers/trip.controller.js
@@ -35,3 +35,79 @@ exports.getTripsByGuideId = async (req, res, next) => {
         res.sendStatus(400);
     }
 }
+
+exports.getParticipantsByTripId = async (req, res, next) => {
+    const tripId = req.params.id;
+
+  try {
+    // Find the trip by ID
+    const trip = await Trip.findOne({ tripId });
+
+    if (!trip) {
+      return res.status(404).json({ message: 'Trip not found' });
+    }
+
+    // Extract participants from the travelersList array
+    const participants = trip.travelersList;
+
+    res.status(200).json({ participants });
+  } catch (error) {
+    console.error('Error fetching participants:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
+exports.getRouteByTripId = async (req, res, next) => {
+    const tripId = req.params.id;
+  
+    try {
+      // Find the trip by ID
+      const trip = await Trip.findOne({ tripId });
+  
+      if (!trip) {
+        return res.status(404).json({ message: 'Trip not found' });
+      }
+  
+      // Extract route information from the route array
+      const route = trip.route;
+  
+      res.status(200).json({ route });
+    } catch (error) {
+      console.error('Error fetching route:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  }
+
+  
+
+  exports.getParticipantsByTripId = async (req, res, next) => {
+    const tripId = req.params.id;
+
+  try {
+    // Find the trip by ID
+    const trip = await Trip.findOne({ tripId });
+
+    if (!trip) {
+      return res.status(404).json({ message: 'Trip not found' });
+    }
+
+    // Extract participants from the travelersList array
+    const participants = trip.travelersList;
+
+    res.status(200).json({ participants });
+  } catch (error) {
+    console.error('Error fetching participants:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
+
+exports.editTrip = async (req, res, next) => {
+    try {
+        const trip = await Trip.updateOne({ _id: req.params.editId }, req.body);
+        res.json(trip);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(400).send(error);
+    }
+};
--- server/routes/trip.routes.js
+++ server/routes/trip.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getTripByGuideId, getTripByTripId} = require("../controllers/trip.controller");
+const { getTripByGuideId, getTripsByTripId, editTrip, getParticipantsByTripId, getRouteByTripId, getEquipmentListByTripId} = require("../controllers/trip.controller");
 const { auth, authNoPermistion } = require("../middlewares/auth");
 const router = express.Router();
 
@@ -9,10 +9,17 @@ const router = express.Router();
 
 router.get("/getTripByGuideId/:id", getTripByGuideId);
 router.get("/getTripsByTripId/:id", getTripsByTripId);
-//router.get("/getTripsByTripId/:id", getTripsByTripId);
-
+router.get('/getParticipantsByTripId/:id', getParticipantsByTripId);
+router.get('/getRouteByTripId/:id', getRouteByTripId);
+router.get('/getEquipmentListByTripId/:id', getEquipmentListByTripId);
+router.put("/editTrip/:id",auth(),editTrip);
 
 
 
 module.exports = router;
+
+
+
+
+
 
