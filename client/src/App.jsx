017e8fc15976eee1a8f1f7aff1646e02a58f9acb
yael <y0583246419@gmail.com>
2023-12-27 13:27:16 +0200

    add tripList, myTrip, travelersList components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,38 @@
 import './App.css'
-import { Provider } from './context/resumeContext'
-import Navbar from './components/navbar'
-import AppRoutes from './components/navbar/AppRoutes'
-import { BrowserRouter } from 'react-router-dom'
+import MyTrips from './components/myTrips/MyTrips';
+// import { Provider } from './context/resumeContext'
+// import Navbar from './components/navbar'
+// import AppRoutes from './components/navbar/AppRoutes'
+// import { BrowserRouter } from 'react-router-dom'
 
 function App() {
 
-  return (
-    <>
-      <Provider>
-        <BrowserRouter>
-          {/* <Navbar /> */}
-          <AppRoutes />
-        </BrowserRouter>
-      </Provider>
-    </>
+  // return (
+  //   <>
+  //     <Provider>
+  //       <BrowserRouter>
+  //         {/* <Navbar /> */}
+  //         <AppRoutes />
+  //       </BrowserRouter>
+  //     </Provider>
+  //   </>
 
-  )
+  // )
+  
+
+
+  const trips = [
+    { tripId: 1, tripName: 'Trip 1', tripDate: '2023-01-01' },
+    { tripId: 2, tripName: 'Trip 2', tripDate: '2023-02-01' },
+    // Add more trips as needed
+  ];
+
+  return (
+    <div>
+      {/* Other components or content */}
+      <MyTrips trips={trips} />
+    </div>
+  );
 }
 
 export default App
---
+++ client/src/components/myTrips/MyTrips.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import TripLine from '../tripLine/TripLine';
+
+const MyTrips = ({ trips }) => {
+  return (
+    <div>
+      <h2>הטיולים שלי</h2>
+      <ul>
+        {trips.map((trip) => (
+            <TripLine trip={trip}></TripLine>
+        ))}
+      </ul>
+      <button onClick={() => console.log("לשים כאן פונקציה")}>צור טיול חדש</button>
+    </div>
+  );
+};
+
+export default MyTrips;
+
