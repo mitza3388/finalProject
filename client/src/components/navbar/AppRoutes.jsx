37a0d5bcf41b3a93451d0bfb81dc60e2ca502b05
mitza3388 <mitza3388@gmail.com>
2024-01-01 11:32:49 +0200

    name changes

--- client/src/components/navbar/AppRoutes.jsx
+++ client/src/components/navbar/AppRoutes.jsx
@@ -4,10 +4,10 @@ import {Route, Routes } from 'react-router-dom'
 import Home from '../../pages/Index'
 import Login from '../../pages/Login'
 import Register from '../../pages/Register'
-import Form from '../form/Form'
 import EntryAs from '../entryAs/EntryAs'
 import GuideEntryPage from '../guideEntryPage/GuideEntryPage'
-import TravelerTrips from '../Traveler/Traveler'
+import Traveler from '../Traveler/Traveler'
+import CreateNewTrip from '../../pages/createNewTrip/CreateNewTrip'
 // import ResumesList from '../../pages/resumeList/ResumesList'
 
 
@@ -19,7 +19,9 @@ export default function AppRoutes() {
       <Route path='/register' element={<EntryAs/>}/>
       <Route path='/login' element={<EntryAs/>}/>
       <Route path='/guide' element={<GuideEntryPage/>}/>
-      <Route path='/traveler' element={<TravelerTrips/>}/>
+      <Route path='/traveler' element={<Traveler/>}/>
+      <Route path='/createNewTrip' element={<CreateNewTrip/>}/>
+      
       {/* <Route path='/form' element={<Form/>}/>
       <Route path='/display' element={<Resume />}/>
       <Route path='/ResumesList' element={<ResumesList />}/> */}
