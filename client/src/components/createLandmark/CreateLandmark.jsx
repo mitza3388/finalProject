6bd0c949b0254d812e8a35ae709978dbde7fc78a
yael <y0583246419@gmail.com>
2024-01-07 10:57:32 +0200

    add createLandmarkModal

--- client/src/components/createLandmark/CreateLandmark.jsx
+++ client/src/components/createLandmark/CreateLandmark.jsx
@@ -258,6 +258,175 @@
 // export default CreateLandmark;
 
 
+/***************** */
+
+// import React, { useState,useEffect } from 'react';
+// import Box from '@mui/material/Box';
+// import TextField from '@mui/material/TextField';
+// import InputLabel from '@mui/material/InputLabel';
+// import { useNavigate } from 'react-router-dom';
+// import LocationSelector from '../locationSelector/LocationSelector';
+// import { Button } from '@mui/material';
+// import fetchData from '../../utils/fetchData';
+// import { useTripContext } from '../../context/tripsContext';
+
+// const CreateLandmark = () => {
+
+//     const { trip, updateTrip } = useTripContext();
+//     const navigate = useNavigate();
+//     const routes = [];
+//     // State to manage form data
+//     const [formData, setFormData] = useState({
+//         location: '',
+//         landmarkName: '',
+//         description: '',
+//         startTime: '',
+//         length: '',
+//         nextLandmarkId: 0,
+//     });
+
+// //     useEffect(() => {
+// //    console.log(trip);
+// //     }, [trip])
+    
+//     // State to manage clicked location
+//     const [clickedLocation, setClickedLocation] = useState(null);
+
+//     // Handle input changes
+//     const handleInputChange = (e) => {
+//         const { name, value } = e.target;
+//         setFormData((prevData) => ({ ...prevData, [name]: value }));
+//     };
+
+//     // Handle clicked location changes
+//     const handleLocationChange = (location) => {
+//         setClickedLocation(location);
+
+//         // Update the location and landmarkName inputs in the form
+//         setFormData((prevData) => ({
+//             ...prevData,
+//             location: `${location.lat.toFixed(4)}, ${location.lng.toFixed(4)}`,
+//             landmarkName: location.streetName,
+//         }));
+//     };
+
+//     // Handle form submission
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+
+//         // Your form submission logic here
+
+//         console.log('Form data submitted:', formData);
+
+
+//         // try {
+//         //     const response = await fetchData(`/addNewLandmark/${id}`, 'PUT', formData);
+//         //     console.log(response);
+//         //     setData(response);
+//         // } catch (error) {
+//         //     console.error('Error:', error.message);
+//         //     setError(error);
+//         // }
+
+//         console.log('form data is', formData);
+
+//         routes.push(formData);
+
+//         console.log('routes is',routes);
+
+//         updateTrip('route', [...routes]);
+
+
+//         console.log('trip is', trip);
+
+//         setFormData({
+//             location: '',
+//             landmarkName: '',
+//             description: '',
+//             startTime: '',
+//             length: '',
+//             nextLandmarkId: 0,
+//         });
+//     };
+
+//     return (
+//         <Box
+//             component="form"
+//             sx={{
+//                 '& .MuiTextField-root': { m: 1, width: '25ch' },
+//                 '& .MuiInputLabel-root': { position: 'absolute' },
+//             }}
+//             noValidate
+//             autoComplete="off"
+//         >
+//             <div className='d-flex row'>
+//                 <div>
+//                     <TextField
+//                         id="landmark-name"
+//                         name="landmarkName"
+//                         label="Landmark Name"
+//                         multiline
+//                         value={formData.landmarkName}
+//                         onChange={handleInputChange}
+//                     />
+//                     <br />
+//                     <TextField
+//                         id="location"
+//                         name="location"
+//                         label="Location"
+//                         multiline
+//                         value={formData.location}
+//                         onChange={handleInputChange}
+//                     />
+//                     <br />
+//                     <TextField
+//                         id="description"
+//                         name="description"
+//                         label="Description"
+//                         placeholder="Description"
+//                         multiline
+//                         value={formData.description}
+//                         onChange={handleInputChange}
+//                     />
+//                     <br />
+//                     <TextField
+//                         id="start-time"
+//                         name="startTime"
+//                         label="Start Time"
+//                         multiline
+//                         value={formData.startTime}
+//                         onChange={handleInputChange}
+//                     />
+//                     <br />
+//                     <TextField
+//                         id="length"
+//                         name="length"
+//                         label="Length"
+//                         multiline
+//                         value={formData.length}
+//                         onChange={handleInputChange}
+//                     />
+//                     <br />
+
+//                     <LocationSelector onLocationChange={handleLocationChange} />
+
+//                     <Button type="submit" onClick={handleSubmit}>
+//                         save
+//                     </Button>
+//                 </div>
+//                 <div>
+//                     {/* Additional content can be added here if needed */}
+//                 </div>
+//             </div>
+//         </Box>
+//     );
+// };
+
+// export default CreateLandmark;
+
+
+/****************** */
+
 
 
 import React, { useState,useEffect } from 'react';
@@ -270,7 +439,7 @@ import { Button } from '@mui/material';
 import fetchData from '../../utils/fetchData';
 import { useTripContext } from '../../context/tripsContext';
 
-const CreateLandmark = () => {
+const CreateLandmark = ({handleClose}) => {
 
     const { trip, updateTrip } = useTripContext();
     const navigate = useNavigate();
@@ -308,6 +477,7 @@ const CreateLandmark = () => {
             location: `${location.lat.toFixed(4)}, ${location.lng.toFixed(4)}`,
             landmarkName: location.streetName,
         }));
+        handleClose;
     };
 
     // Handle form submission
@@ -411,7 +581,7 @@ const CreateLandmark = () => {
                     <LocationSelector onLocationChange={handleLocationChange} />
 
                     <Button type="submit" onClick={handleSubmit}>
-                        Create Landmark
+                      save
                     </Button>
                 </div>
                 <div>
--- client/src/pages/createNewTrip/CreateNewTrip.jsx
+++ client/src/pages/createNewTrip/CreateNewTrip.jsx
@@ -1,18 +1,470 @@
-import React from 'react';
+// import React from 'react';
+// import fetchData from '../../utils/fetchData';
+// import { useNavigate } from 'react-router-dom';
+// import { Button, FloatingLabel, Form } from 'react-bootstrap';
+// import './createNewTrip.css';
+// import { useTripContext } from '../../context/tripsContext';
+// import LandmarkModal from '../../components/landmarkModal/LandmarkModal';
+
+// const CreateNewTrip = () => {
+//   const navigate = useNavigate();
+//   const { trip, updateTrip } = useTripContext();
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+  
+//     // Update the trip context dynamically as the form changes
+//     updateTrip(name, value);
+//   };
+    
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const response = await fetchData('trips/addNewTrip', 'POST', trip);
+//       console.log(response);
+//       if (!response) {
+//         console.log('Bad Request');
+//       } else {
+//         const newTrip = response.data; // Adjust this based on your API response
+//         updateTrip(newTrip); // Update the context with the newly created trip
+//         navigate('/guide');
+//       }
+//     } catch (error) {
+//       console.error('Error:', error.message);
+//     }
+//   };
+
+
+//   // return (
+//   //   <div className="box">
+//   //     <h2>Create new Trip</h2>
+//   //     <form id="registerForm" onSubmit={handleSubmit}>
+//   //       <label htmlFor="registerEmail">
+//   //         Email
+//   //         <input id="registerEmail"
+//   //           type="email"
+//   //           name="email"
+//   //           value={formData.email}
+//   //           onChange={handleChange}
+//   //         />
+//   //       </label>
+//   //       <br />
+//   //       <label htmlFor="registerPassword">
+//   //         Password
+//   //         <input id="registerPassword"
+//   //           type="password"
+//   //           name="password"
+//   //           value={formData.password}
+//   //           onChange={handleChange}
+//   //         />
+//   //       </label>
+//   //       <br />
+//   //       <button type="submit">Register</button>
+//   //     </form>
+//   //     {error && <h5 style={{ color: 'red' }}>user is already exist</h5>} 
+//   //   </div>
+//   // );
+//   return (
+//     <div className='container' style={styles.container}>
+//       <form onSubmit={handleSubmit} style={styles.form}>
+//         <h2>Add new Trip</h2>
+//         <div style={styles.inputGroup}>
+//           <FloatingLabel controlId="floating input" label="Trip name" className="my-4 w-75">
+//             <Form.Control
+//               type="text"
+//               placeholder="Trip name"
+//               name='tripName'
+//               value={trip.tripName}
+//               onChange={handleChange}
+//             />
+//           </FloatingLabel>
+
+//           <div>
+//             <div className='d-flex justify-content-around'>
+//               <Button
+//                 className='m-3 bg-danger px-3'
+//                 onClick={() => navigate('/createEquipmentList')}>
+//                 Add Equipment List
+//               </Button>
+
+//               <Button
+//                 className='m-3 bg-danger px-3'
+//                 onClick={() => navigate('/createEquipmentList')}>
+//                 Add --- List
+//               </Button>
+
+//               <Button
+//                 className='m-3 bg-danger'
+//                 onClick={() => navigate('/createLandmark')}
+//               >
+//                 Add Landmark</Button>
+//             </div>
+
+//             <Button type="submit" className='w-100'>
+//               Submit
+//             </Button>
+//           </div>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
+// {/* <label style={styles.label} htmlFor="tripName">
+//           </label>
+//           <input
+//             placeholder='Trip name'
+//             type="text"
+//             id="tripName"
+//             name="tripName"
+//             value={formData.tripName}
+//             onChange={handleChange}
+//             style={styles.input}
+//           /> */}
+
+// {/* <div style={styles.inputGroup}>
+//           <label style={styles.label} htmlFor="tripDesc">
+//             Trip Description:
+//           </label>
+//           <textarea   
+//             id="tripDesc"
+//             name="tripDesc"
+//             value={formData.tripDesc}
+//             onChange={handleChange}
+//             style={styles.input}
+//           ></textarea>
+//         </div>
+
+//         <div style={styles.inputGroup}>
+//           <label style={styles.label} htmlFor="tripDate">
+//             Choose Date:
+//           </label>
+//           <input
+//             type="date"
+//             id="tripDate"
+//             name="tripDate"
+//             value={formData.tripDate}
+//             onChange={handleChange}
+//             style={styles.input}
+//           />
+//   </div>*/}
+
+
+
+
+// const styles = {
+//   container: {
+//     padding: '20px',
+//     display: 'flex',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     height: '100vh',
+//   },
+//   form: {
+//     width: '600px',
+//     padding: '20px',
+//     border: '1px solid #ccc',
+//     borderRadius: '8px',
+//     boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+//     background: '#fff',
+//   },
+//   inputGroup: {
+//     marginBottom: '15px',
+//   },
+//   label: {
+//     display: 'block',
+//     marginBottom: '5px',
+//     fontWeight: 'bold',
+//   },
+//   input: {
+//     width: '100%',
+//     padding: '8px',
+//     border: '1px solid #ccc',
+//     borderRadius: '4px',
+//   },
+//   buttonGroup: {
+//     display: 'flex',
+//     justifyContent: 'space-between',
+//     marginBottom: '15px',
+//   },
+//   button: {
+//     flex: '1',
+//     padding: '8px',
+//     border: 'none',
+//     borderRadius: '4px',
+//     cursor: 'pointer',
+//     transition: 'background-color 0.3s ease',
+//   },
+//   submitButton: {
+//     backgroundColor: '#3498db',
+//     color: '#fff',
+//     width: '100%',
+//     padding: '8px',
+//     border: 'none',
+//     borderRadius: '4px',
+//     cursor: 'pointer',
+//     transition: 'background-color 0.3s ease',
+//   },
+// };
+
+// export default CreateNewTrip;
+
+
+
+/****************************** */
+
+// import React, { useState } from 'react';
+// import fetchData from '../../utils/fetchData';
+// import { useNavigate } from 'react-router-dom';
+// import { Button, FloatingLabel, Form } from 'react-bootstrap';
+// import './createNewTrip.css';
+// import LandmarkModal from '../../components/landmarkModal/LandmarkModal';
+// import { useTripContext } from '../../context/tripsContext';
+
+// const CreateNewTrip = () => {
+//   const navigate = useNavigate();
+//   const { trip, updateTrip } = useTripContext();
+//   const [showModal, setShowModal] = useState(false);
+
+//   const handleShowModal = () => setShowModal(true);
+//   const handleCloseModal = () => setShowModal(false);
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     updateTrip(name, value);
+//   };
+    
+
+//   /**** */
+//   const handleSave = () => {
+//     // Handle save logic here
+//     // This function will be called when the "Save" button is clicked in the modal
+//     console.log('Saving landmark...');
+//     handleCloseModal();
+//   };
+//   /***** */
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const response = await fetchData('trips/addNewTrip', 'POST', trip);
+//       console.log(response);
+//       if (!response) {
+//         console.log('Bad Request');
+//       } else {
+//         const newTrip = response.data; // Adjust this based on your API response
+//         updateTrip(newTrip); // Update the context with the newly created trip
+//         navigate('/guide');
+//       }
+//       handleShowModal();
+//     } catch (error) {
+//       console.error('Error:', error.message);
+//     }
+  
+//   };
+
+
+//   // return (
+//   //   <div className="box">
+//   //     <h2>Create new Trip</h2>
+//   //     <form id="registerForm" onSubmit={handleSubmit}>
+//   //       <label htmlFor="registerEmail">
+//   //         Email
+//   //         <input id="registerEmail"
+//   //           type="email"
+//   //           name="email"
+//   //           value={formData.email}
+//   //           onChange={handleChange}
+//   //         />
+//   //       </label>
+//   //       <br />
+//   //       <label htmlFor="registerPassword">
+//   //         Password
+//   //         <input id="registerPassword"
+//   //           type="password"
+//   //           name="password"
+//   //           value={formData.password}
+//   //           onChange={handleChange}
+//   //         />
+//   //       </label>
+//   //       <br />
+//   //       <button type="submit">Register</button>
+//   //     </form>
+//   //     {error && <h5 style={{ color: 'red' }}>user is already exist</h5>} 
+//   //   </div>
+//   // );
+//   return (
+//     <div className='container' style={styles.container}>
+//       <form onSubmit={handleSubmit} style={styles.form}>
+//         <h2>Add new Trip</h2>
+//         <div style={styles.inputGroup}>
+//           <FloatingLabel controlId="floating input" label="Trip name" className="my-4 w-75">
+//             <Form.Control
+//               type="text"
+//               placeholder="Trip name"
+//               name='tripName'
+//               value={trip.tripName}
+//               onChange={handleChange}
+//             />
+//           </FloatingLabel>
+
+//           <div>
+//             <div className='d-flex justify-content-around'>
+//               <Button
+//                 className='m-3 bg-danger px-3'
+//                 onClick={() => navigate('/createEquipmentList')}>
+//                 Add Equipment List
+//               </Button>
+
+//               <Button
+//                 className='m-3 bg-danger px-3'
+//                 onClick={() => navigate('/createEquipmentList')}>
+//                 Add --- List
+//               </Button>
+
+//               <Button className='m-3 bg-danger' onClick={handleShowModal}>
+//             Add Landmark
+//           </Button>
+//             </div>
+
+//             <Button type="submit" className='w-100'>
+//               Submit
+//             </Button>
+//           </div>
+//         </div>
+//       </form>
+
+//       <LandmarkModal show={showModal} handleClose={handleCloseModal} handleSave={handleSave}/>
+//     </div>
+//   );
+// };
+// {/* <label style={styles.label} htmlFor="tripName">
+//           </label>
+//           <input
+//             placeholder='Trip name'
+//             type="text"
+//             id="tripName"
+//             name="tripName"
+//             value={formData.tripName}
+//             onChange={handleChange}
+//             style={styles.input}
+//           /> */}
+
+// {/* <div style={styles.inputGroup}>
+//           <label style={styles.label} htmlFor="tripDesc">
+//             Trip Description:
+//           </label>
+//           <textarea   
+//             id="tripDesc"
+//             name="tripDesc"
+//             value={formData.tripDesc}
+//             onChange={handleChange}
+//             style={styles.input}
+//           ></textarea>
+//         </div>
+
+//         <div style={styles.inputGroup}>
+//           <label style={styles.label} htmlFor="tripDate">
+//             Choose Date:
+//           </label>
+//           <input
+//             type="date"
+//             id="tripDate"
+//             name="tripDate"
+//             value={formData.tripDate}
+//             onChange={handleChange}
+//             style={styles.input}
+//           />
+//   </div>*/}
+
+
+
+
+// const styles = {
+//   container: {
+//     padding: '20px',
+//     display: 'flex',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     height: '100vh',
+//   },
+//   form: {
+//     width: '600px',
+//     padding: '20px',
+//     border: '1px solid #ccc',
+//     borderRadius: '8px',
+//     boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+//     background: '#fff',
+//   },
+//   inputGroup: {
+//     marginBottom: '15px',
+//   },
+//   label: {
+//     display: 'block',
+//     marginBottom: '5px',
+//     fontWeight: 'bold',
+//   },
+//   input: {
+//     width: '100%',
+//     padding: '8px',
+//     border: '1px solid #ccc',
+//     borderRadius: '4px',
+//   },
+//   buttonGroup: {
+//     display: 'flex',
+//     justifyContent: 'space-between',
+//     marginBottom: '15px',
+//   },
+//   button: {
+//     flex: '1',
+//     padding: '8px',
+//     border: 'none',
+//     borderRadius: '4px',
+//     cursor: 'pointer',
+//     transition: 'background-color 0.3s ease',
+//   },
+//   submitButton: {
+//     backgroundColor: '#3498db',
+//     color: '#fff',
+//     width: '100%',
+//     padding: '8px',
+//     border: 'none',
+//     borderRadius: '4px',
+//     cursor: 'pointer',
+//     transition: 'background-color 0.3s ease',
+//   },
+// };
+
+// export default CreateNewTrip;
+
+
+/**************** */
+
+
+
+
+
+
+
+
+
+
+import React, { useState } from 'react';
 import fetchData from '../../utils/fetchData';
 import { useNavigate } from 'react-router-dom';
 import { Button, FloatingLabel, Form } from 'react-bootstrap';
 import './createNewTrip.css';
+import LandmarkModal from '../../components/landmarkModal/LandmarkModal';
 import { useTripContext } from '../../context/tripsContext';
 
 const CreateNewTrip = () => {
   const navigate = useNavigate();
   const { trip, updateTrip } = useTripContext();
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-  
-    // Update the trip context dynamically as the form changes
     updateTrip(name, value);
   };
     
@@ -31,6 +483,7 @@ const CreateNewTrip = () => {
     } catch (error) {
       console.error('Error:', error.message);
     }
+    handleShowModal();
   };
 
 
@@ -92,11 +545,9 @@ const CreateNewTrip = () => {
                 Add --- List
               </Button>
 
-              <Button
-                className='m-3 bg-danger'
-                onClick={() => navigate('/createLandmark')}
-              >
-                Add Landmark</Button>
+              <Button className='m-3 bg-danger' onClick={handleShowModal}>
+            Add Landmark
+          </Button>
             </div>
 
             <Button type="submit" className='w-100'>
@@ -105,6 +556,8 @@ const CreateNewTrip = () => {
           </div>
         </div>
       </form>
+
+      <LandmarkModal show={showModal} handleClose={handleCloseModal} />
     </div>
   );
 };
@@ -206,3 +659,4 @@ const styles = {
 };
 
 export default CreateNewTrip;
+
