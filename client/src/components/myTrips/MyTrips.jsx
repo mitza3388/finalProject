d12697e426135f79df208be2e68a5d6a9465f1ac
yael <y0583246419@gmail.com>
2024-01-11 12:27:07 +0200

    add JoinTripConfirmation

--- client/src/components/myTrips/MyTrips.jsx
+++ client/src/components/myTrips/MyTrips.jsx
@@ -1,45 +1,43 @@
-import React from 'react';
-import TripLine from '../tripLine/TripLine';
-
-const MyTrips = ({ trips }) => {
-
-  return (
-    <div>
-      <h2>My Trips</h2>
-      <ul>
-        {trips?.map((trip, index) => (
-          <TripLine key={index} trip={trip}></TripLine>
-        ))}
-      </ul>
-      {/* <button onClick={() => console.log("לשים כאן פונקציה")}>צור טיול חדש</button> */}
-    </div>
-  );
-};
-
-export default MyTrips;
-
-
-
 // import React from 'react';
 // import TripLine from '../tripLine/TripLine';
-// import './myTrips.css'; // Import the CSS file for styling
-// import SideNavbar from '../sideNavbar/SideNavbar';
 
 // const MyTrips = ({ trips }) => {
+
 //   return (
-//     <>
-//     <SideNavbar/>
-//     <div className="my-trips-container">
-//       <h2 className="my-trips-title">My Trips</h2>
-//       <ul className="my-trips-list">
+//     <div>
+//       <h2>My Trips</h2>
+//       <ul>
 //         {trips?.map((trip, index) => (
 //           <TripLine key={index} trip={trip}></TripLine>
 //         ))}
 //       </ul>
 //       {/* <button onClick={() => console.log("לשים כאן פונקציה")}>צור טיול חדש</button> */}
 //     </div>
-//     </>
 //   );
 // };
 
 // export default MyTrips;
+
+
+
+import React from 'react';
+import TripLine from '../tripLine/TripLine';
+import { Row, Col } from 'react-bootstrap';
+
+const MyTrips = ({ trips }) => {
+  return (
+    <div>
+      <h2>My Trips</h2>
+      <Row xs={1} md={3} className="g-4">
+        {trips?.map((trip, index) => (
+          <Col key={index}>
+            <TripLine trip={trip}></TripLine>
+          </Col>
+        ))}
+      </Row>
+      {/* <button onClick={() => console.log("לשים כאן פונקציה")}>צור טיול חדש</button> */}
+    </div>
+  );
+};
+
+export default MyTrips;
--- client/src/components/tripLine/TripLine.jsx
+++ client/src/components/tripLine/TripLine.jsx
@@ -1,20 +1,87 @@
 
-// import React from 'react'
 
+
+// import React, { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
 
 // const TripLine = ({ trip }) => {
+//   const [isHovered, setIsHovered] = useState(false);
+//   const navigate = useNavigate();
 
-//     return (
-//         <div key={trip.tripId}>
-//             <span>{trip.tripName} </span>
-//             <span>{trip.tripDate} </span>
-//             <button onClick={() => console.log(`Add button clicked for ${trip.tripName}`)}>+</button>
-//         </div>
-//     )
-// }
-
+//   return (
+//     <div
+//       key={trip.tripId}
+//       onClick={() => navigate("/viewTrip")}
+//       onMouseEnter={() => setIsHovered(true)}
+//       onMouseLeave={() => setIsHovered(false)}
+//       style={{
+//         margin: '20px',
+//         minHeight: '30px',
+//         cursor: 'pointer', // Change cursor on hover
+//         color: isHovered ? '#e0e0e0' : 'transparent', // Change background color on hover
+//         transition: 'color 0.3s', // Add smooth transition
+//         fontSize: '30px'
+//       }}
+//     >
+//       <img
+//         src="https://cdn-icons-png.flaticon.com/128/703/703320.png?ga=GA1.1.707984635.1704275258&semt=ais"
+//         alt="Add icon"
+//         style={{ marginRight: '5px', width: '40px', height: '40px', fontSize: '20' }}
+//       />
+//       <span style={{ color: isHovered ? '#184655' : 'black' }}>{trip.tripName} </span>
+//       <span>{trip.tripDate} </span>
+//     </div>
+//   );
+// };
 
 // export default TripLine;
+
+
+
+// import React, { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+
+// const TripLine = ({ trip }) => {
+//   const [isHovered, setIsHovered] = useState(false);
+//   const navigate = useNavigate();
+
+//   return (
+//     <div
+//       key={trip.tripId}
+//       onClick={() => navigate("/viewTrip")}
+//       onMouseEnter={() => setIsHovered(true)}
+//       onMouseLeave={() => setIsHovered(false)}
+//       style={{
+//         margin: '20px',
+//         cursor: 'pointer',
+//         transition: 'color 0.3s',
+//         fontSize: '30px',
+//       }}
+//     >
+//       <div
+//         style={{
+//           padding: '20px',
+//           borderRadius: '10px',
+//           boxShadow: isHovered ? '0 10px 15px black' : '0 0 5px rgba(0, 0, 0, 0.1)',
+//           background: isHovered ? '#e0e0e0' : 'transparent',
+//           transition: 'background 0.3s, box-shadow 0.3s',
+          
+//         }}
+//       >
+//         <img
+//           src="https://cdn-icons-png.flaticon.com/128/703/703320.png?ga=GA1.1.707984635.1704275258&semt=ais"
+//           alt="Add icon"
+//           style={{ marginRight: '5px', width: '40px', height: '40px', fontSize: '20' }}
+//         />
+//         <span style={{ color: isHovered ? '#184655' : 'black' }}>{trip.tripName} </span>
+//         <span>{trip.tripDate} </span>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default TripLine;
+
 
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -31,20 +98,28 @@ const TripLine = ({ trip }) => {
       onMouseLeave={() => setIsHovered(false)}
       style={{
         margin: '20px',
-        minHeight: '30px',
-        cursor: 'pointer', // Change cursor on hover
-        color: isHovered ? '#e0e0e0' : 'transparent', // Change background color on hover
-        transition: 'color 0.3s', // Add smooth transition
-        fontSize: '30px'
+        cursor: 'pointer',
+        transition: 'color 0.3s',
+        fontSize: '30px',
       }}
     >
-      <img
-        src="https://cdn-icons-png.flaticon.com/128/703/703320.png?ga=GA1.1.707984635.1704275258&semt=ais"
-        alt="Add icon"
-        style={{ marginRight: '5px', width: '40px', height: '40px', fontSize: '20' }}
-      />
-      <span style={{ color: isHovered ? '#184655' : 'black' }}>{trip.tripName} </span>
-      <span>{trip.tripDate} </span>
+      <div
+        style={{
+          padding: '20px',
+          borderRadius: '10px',
+          boxShadow: isHovered ? '0 10px 15px black' : '0 0 5px rgba(0, 0, 0, 0.1)',
+          background: isHovered ? 'rgba(245, 245, 220, 0.51)' : 'transparent',
+          transition: 'background 0.3s, box-shadow 0.3s',
+        }}
+      >
+        <img
+          src="https://cdn-icons-png.flaticon.com/128/703/703320.png?ga=GA1.1.707984635.1704275258&semt=ais"
+          alt="Add icon"
+          style={{ marginRight: '5px', width: '40px', height: '40px', fontSize: '20' }}
+        />
+        <span style={{ color: isHovered ? '#184655' : 'black' }}>{trip.tripName} </span>
+        <span>{trip.tripDate} </span>
+      </div>
     </div>
   );
 };
