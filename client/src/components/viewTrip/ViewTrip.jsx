9bfb8558a329bf7dca8b556f10028f439865d976
yael <y0583246419@gmail.com>
2024-01-13 22:38:39 +0200

    Correct the context in Tri[Line and Viewtrip

--- client/src/components/viewTrip/ViewTrip.jsx
+++ client/src/components/viewTrip/ViewTrip.jsx
@@ -1,24 +1,56 @@
+// import React from 'react';
+// import TravelerChips from '../travelerChips/TravelerChips';
+// import LandmarkTimeline from '../landmarkTimeline/LandmarkTimeline';
+// import { useTripContext } from '../../context/tripsContext';
+
+// const ViewTrip = () => {
+//   const { trip } = useTripContext();
+//   console.log("trip in view trip is",trip);
+
+//   if (!trip) {
+//     return <div>No trip data available</div>;
+//   }
+
+// const landmarks = trip.route;
+// console.log(trip.route);
+
+//   return (
+//      <div>
+//       <h1>{`${trip.tripName}`}</h1>
+//       <LandmarkTimeline landmarks={landmarks}></LandmarkTimeline>
+//       {/* <WaypointsAccordion></WaypointsAccordion>*/} 
+//         <TravelerChips></TravelerChips> 
+//     </div>
+//   );
+// }
+
+// export default ViewTrip;
+
+
 import React from 'react';
 import TravelerChips from '../travelerChips/TravelerChips';
 import LandmarkTimeline from '../landmarkTimeline/LandmarkTimeline';
-import { useTripContext } from '../../context/tripsContext';
+import { useLocation } from 'react-router-dom';
 
 const ViewTrip = () => {
-  const { trip } = useTripContext();
-  console.log("trip in view trip is",trip);
+  const location = useLocation();
+  const trip = location.state?.trip;
+
+  console.log("trip in view trip is", trip);
 
   if (!trip) {
     return <div>No trip data available</div>;
   }
 
-const landmarks = trip.route;
-console.log(trip.route);
+  const landmarks = trip.route;
+  console.log(trip.route);
 
   return (
-     <div>
+    <div>
+      <h1>{`${trip.tripName}`}</h1>
       <LandmarkTimeline landmarks={landmarks}></LandmarkTimeline>
-      {/* <WaypointsAccordion></WaypointsAccordion>*/} 
-        <TravelerChips></TravelerChips> 
+      {/* <WaypointsAccordion></WaypointsAccordion>*/}
+      <TravelerChips></TravelerChips>
     </div>
   );
 }
